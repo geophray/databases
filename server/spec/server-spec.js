0583f49d22ef9cbec2e7b338f499ed6cac26ba1e
Jeff Maughan <geophray@gmail.com>
2020-04-16 18:28:20 -0700

    Update last test based on our schema

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -68,8 +68,9 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-    var queryString = '';
-    var queryArgs = [];
+    var queryString = 'INSERT INTO messages (text, id_users, id_rooms) VALUES (?, ?, ?)';
+    var queryArgs = ['Men like you can never change!', 1, 1 ];
+
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
@@ -82,7 +83,7 @@ describe('Persistent Node Chat Server', function() {
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
         expect(messageLog[0].text).to.equal('Men like you can never change!');
-        expect(messageLog[0].roomname).to.equal('main');
+        expect(messageLog[0].roomname).to.equal(1);
         done();
       });
     });
